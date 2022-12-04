3886dbea2ee51c91bd3c138325ad013128a1995c
danielgregoire34 <danielgregoire34@gmail.com>
2022-12-04 16:49:17 -0500

    adding to the index.js file some of the title and functionality to the readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+inquirer
+.prompt([
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is your title?',
+    },
+    
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Description of your application?',
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'Installation Instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Information?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Contribution Guidelines?',
+            },
+            {
+                type: 'input',
+                name: 'testinstallation',
+                message: 'Test Instructions?',
+                },
+
+])
+.then((data) => {
+    const filename = `index.md`;
+
+    fs.writeFile(filename, writeToFile(data), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+});
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+/*
+function writeToFile(fileName, data) {
+
+}
+*/
+
+let writeToFile = (data) => {
+    return `#${data.title}
+    
+    ##Description
+    ${data.description}
+
+
+    ##Installation Instructions
+    ${data.installation}
+
+    
+    ##Usage Information
+    ${data.usage}
+
+
+    ##Contribution Guidlines 
+    ${data.contribution}
+
+
+    ##Test Installation 
+    ${data.testinstallation}
+    
+    
+    
+    
+    `
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
